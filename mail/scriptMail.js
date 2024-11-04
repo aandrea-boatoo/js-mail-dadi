a97cd70f4dc09c6b4bd61e6e6cb80b53581e1f41
Boato Andrea <163646394+aandrea-boatoo@users.noreply.github.com>
2024-11-04 15:39:01 +0100

    upload files and start mail ex

---
+++ mail/scriptMail.js
@@ -0,0 +1,34 @@
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+// Non è necessario provvedere alla validazione delle email
+
+// OBBIETTIVO: VERIFICA MAIL
+
+const mailData = [
+    "roberto.bolle@gmail.com",
+    "prima.vera@gmail.com",
+    "andrea.boato@gmail.com",
+    "rodolfoalfonso@gmail.com",
+    "prezzemolo34@gmail.com",
+    "cristanoronaldo7@gmail.com",
+    "therock22@gmail.com",
+    "amazon.info@gmail.com",
+    "complain.prez@gmail.com",
+    "2131241@gmail.com",
+    "avelo.zucchero@gmail.com",
+    "ottone.erminio@gmail.com",
+    "facciada.leva@gmail.com",
+    "prestigiacomo.giacomo@gmail.com",
+    "lorenzorossi@gmail.com"
+]
+
+const search = prompt("inserire indirizzo email");
+
+if (mailData.includes(search)){
+for(i = 0 ; i < mailData.length() ; i++){
+    if (mailData[i] === search){
+        console.log(`email valida, accesso consentito (col ${mailData.indexOf[i]}))`)
+    }
+}
+}
